9c97a021fa12ab5cc4a70acc53fc97ebbbee841f
Kwon-Kiseok <soorison@naver.com>
2022-03-30 16:13:44 +0900

    Implement game loop

--- Sokoban/Sokoban/main.c
+++ Sokoban/Sokoban/main.c
@@ -1,7 +1,13 @@
 ﻿#include "stdafx.h"
+#include "Framework/Game.h"
 
 int main()
 {
-	puts("Hello, World!");
-	return 0;
+	if (false == Initialize())
+	{
+		puts("게임을 로딩하는 데 문제가 생겼습니다.");
+		return 1;
+	}
+
+	return Run();
 }
